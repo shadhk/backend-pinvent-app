a98488021423c6253e422e55f5c63cdb86fa9dac
shadhk <shadhussaink@gmail.com>
2024-03-20 00:12:15 +0530

    Completed backend development

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { registerUser, loginUser, logoutUser, getUser, loginStatus, updateUser, changePassword, forgotPassword, resetPassword } = require("../controllers/userController")
+const protect = require("../middleware/authMiddleware")
+const router = express.Router()
+
+router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.get("/logout", logoutUser)
+router.get("/getuser", protect, getUser)
+router.get("/loggedin", loginStatus)
+router.patch("/updateuser", protect, updateUser)
+router.patch("/changepassword", protect, changePassword)
+router.post("/forgotpassword", forgotPassword)
+router.put("/resetpassword/:resetToken", resetPassword)
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,12 +3,39 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const userRoute = require("./routes/userRoute")
+const productRoute = require("./routes/productRoute")
+const conatctRoute = require("./routes/contactRoute")
+const errorHandler = require("./middleware/errorMiddleware")
+const cookieParser = require("cookie-parser")
+const path = require("path")
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+// Middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
+// Routes Middleware
+app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
+app.use("/api/contactus", conatctRoute)
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page")
+})
+
+// Error Middleware
+app.use(errorHandler)
 
 // Connect to DB and start server
+const PORT = process.env.PORT || 5000
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
